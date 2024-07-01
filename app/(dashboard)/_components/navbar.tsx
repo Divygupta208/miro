6840b84819211c68e25ec69507cb0f83d6ee9f43
Divy Gupta <divygupta208@gmail.com>
2024-07-01 12:26:36 +0530

    navbar and sidebar

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center gap-x-4 p-5 bg-green-500 ">
+      <div className="hidden lg:flex lg:flex-1"></div>
+      <UserButton />
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
