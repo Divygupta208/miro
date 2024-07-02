08900bfe5bdec620dc75a07d7ff1309e8757aae8
Divy Gupta <divygupta208@gmail.com>
2024-07-02 13:43:57 +0530

    board-list component

---
+++ app/(dashboard)/_components/board-list.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Emptyboards } from "./empty-boards";
+import { EmptyFavorites } from "./empty-favorites";
+import { EmptySearch } from "./empty-search";
+
+interface BoardListProps {
+  orgId: string;
+  query: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+export const BoardList = ({ orgId, query }: BoardListProps) => {
+  const data = [];
+
+  if (!data?.length && query.search) {
+    return <EmptySearch />;
+  }
+
+  if (!data?.length && query.favorites) {
+    return <EmptyFavorites />;
+  }
+
+  if (!data?.length) {
+    return <Emptyboards />;
+  }
+
+  return <div>{JSON.stringify(query)}</div>;
+};
