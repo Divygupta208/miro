f11b2b42ac1f78693a51c394bbdbe0175f6e2a49
Divy Gupta <divygupta208@gmail.com>
2024-07-04 11:14:34 +0530

    new boards button

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+"use client";
 
-const Dashboardpage = () => {
-  return <div>page</div>;
+import { useOrganization } from "@clerk/nextjs";
+import { EmptyOrg } from "./_components/empty-org";
+import { BoardList } from "./_components/board-list";
+
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
+  const { organization } = useOrganization();
+
+  return (
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
+    </div>
+  );
 };
 
-export default Dashboardpage;
+export default DashboardPage;
